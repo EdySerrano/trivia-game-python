5307efc6006cf312c9770421ab70adff7e8109a3
edysaul20000 <edysaulserrano@gmail.com>
2025-04-15 16:06:36 -0500

    Dia4 :Sistema de puntuación, manejo de rondas y finalización del juego

--- trivia.py
+++ trivia.py
@@ -1,12 +1,19 @@
 # trivia.py
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 class Question:
     def __init__(self, description, options, correct_answer):
         self.description = description
         self.options = options
         self.correct_answer = correct_answer
 
-    def is_correct(self, answer):
+n    def is_correct(self, answer):
         return self.correct_answer == answer
 
     
